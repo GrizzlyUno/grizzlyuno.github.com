3b5f5abda7185a072ffacb4621203f6fa267fd41
Nocturne Ink <122688392+GrizzlyUno@users.noreply.github.com>
2025-05-23 17:24:31 +0200

    Adding Files From Local

---
+++ func.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    const CurrentYear = new Date() .getFullYear()
+    var Age = CurrentYear - 2006
+    document.getElementById("InfoText").textContent = "I am " + Age + " year old,"
+}
+
+
+
+//let ball = document.getElementById("rotDiv");
+//var myVar = setInterval(spostaDiv, 90);
+//var margin = 0;
+
+//let l = window.screen.width;
+//let w = 1300;
+
+//function spostaDiv() {
+    //console.log(w);
+    //if (margin == w) {
+   //     margin = 0 + "px";
+    //} else {
+   //     ball.style.marginLeft = margin + "px";
+   // }
+    //margin += 1;
+    
+
+//}
